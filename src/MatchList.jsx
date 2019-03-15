20998f15cef9c4bbef6ba39c17d9c91bfd986263
Henry Heikkinen <rce@rce.fi>
2019-03-15 19:28:41 +0200

    Show players in match details

--- src/MatchList.jsx
+++ src/MatchList.jsx
@@ -28,23 +28,26 @@ const Match = ({playerId, matchId}) => {
       {U.cond(
         [match.map(R.isNil), <Spinner />],
         [match.map(m => m.version === 2), <MatchV2 playerId={playerId} match={match} />],
+        [match.map(m => m.version === 1), <MatchV1 playerId={playerId} match={match} />],
         [<p>Unknown match version</p>])}
     </div>
   )
 }
 
-const MatchV2 = ({match, playerId}) => {
+const MatchV1 = ({match, playerId}) => {
+  const getRoster = R.prop("roster_v1")
   const isOpen = new Atom(false)
   const team1 = U.view(["teams", "faction1"], match)
   const team2 = U.view(["teams", "faction2"], match)
 
   const winOrLoss = U.when(match,
     Kefir.combine([match], [playerId], (match, playerId) => {
-      return hasPlayer(playerId, winner(match))
+      return hasPlayer(getRoster)(playerId, winner(match))
         ? <span className="win">WIN</span>
         : <span className="loss">LOSS</span>
     }))
-  const TeamName = mkTeamName(playerId)
+
+  const TeamName = mkTeamName(getRoster)(playerId)
   return (
     <div>
       <div className="match-header" onClick={() => isOpen.modify(R.not)}>
@@ -65,29 +68,93 @@ const MatchV2 = ({match, playerId}) => {
             <i className="fas fa-chevron-right" />)}</p>
         </div>
       </div>
-      {U.when(isOpen, <MatchDetails match={match} />)}
+      {U.when(isOpen, <MatchDetails match={match} getRoster={getRoster} />)}
     </div>
   )
 }
 
-const MatchDetails = ({match}) => {
+const MatchV2 = ({match, playerId}) => {
+  const getRoster = R.prop("roster")
+  const isOpen = new Atom(false)
+  const team1 = U.view(["teams", "faction1"], match)
+  const team2 = U.view(["teams", "faction2"], match)
+
+  const winOrLoss = U.when(match,
+    Kefir.combine([match], [playerId], (match, playerId) => {
+      return hasPlayer(getRoster)(playerId, winner(match))
+        ? <span className="win">WIN</span>
+        : <span className="loss">LOSS</span>
+    }))
+  const TeamName = mkTeamName(getRoster)(playerId)
   return (
-    <div className="match-details">
-      <pre>{U.stringify(match, null, 2)}</pre>
+    <div>
+      <div className="match-header" onClick={() => isOpen.modify(R.not)}>
+        <div className="result">
+          <p>{winOrLoss}</p>
+        </div>
+        <div className="match-name">
+          <p>
+            <TeamName team={team1} /> vs <TeamName team={team2} /> ({U.view("competition_name", match)})
+          </p>
+        </div>
+        <div className="timestamp">
+          <p>{U.view(["started_at", formatTime], match)}</p>
+        </div>
+        <div className="toggle">
+          <p>{U.ifElse(isOpen,
+            <i className="fas fa-chevron-down" />,
+            <i className="fas fa-chevron-right" />)}</p>
+        </div>
+      </div>
+      {U.when(U.lift(R.and)(isOpen, match), <MatchDetails match={match} getRoster={getRoster} />)}
     </div>
   )
 }
+
+const MatchDetails = ({match, getRoster}) => {
+  const showJson = new Atom(false)
+
+  const players = U.lift(team => {
+    return (
+      <div className="team">
+        <img className="avatar" src={U.view("avatar", team)} />
+        <div className="players">
+          <h2>{U.view("name", team)}</h2>
+          <ul>
+            {U.mapElems(
+              (p, i) => <li key={i}>{U.view("nickname", p)}</li>,
+              getRoster(team))}
+          </ul>
+        </div>
+      </div>
+    )
+  })
+
+  return (
+    <div className="match-details">
+      <div className="teams">
+        {players(match.map(R.path(["teams", "faction1"])))}
+        {players(match.map(R.path(["teams", "faction2"])))}
+      </div>
+
+      <button onClick={toggle(showJson)}>Show JSON</button>
+      {U.when(showJson, <pre>{U.stringify(match, null, 2)}</pre>)}
+    </div>
+  )
+}
+
+const toggle = obs => () => obs.modify(R.not)
 
 const winner = match => match.teams[match.results.winner]
 
-const hasPlayer = (playerId, team) =>
-  team.roster.map(R.prop("player_id")).includes(playerId)
+const hasPlayer = getRoster => (playerId, team) =>
+  getRoster(team).map(R.prop("player_id")).includes(playerId)
 
 const formatTime = date => date.toRelative()
 
-const mkTeamName = playerId => ({team, className}) => {
+const mkTeamName = getRoster => playerId => ({team, className}) => {
   const isHomeTeam = team => Kefir.combine([team], [playerId],
-    (team, playerId) => team.roster.map(R.prop("player_id")).includes(playerId))
+    (team, playerId) => getRoster(team).map(R.prop("player_id")).includes(playerId))
   return <span className={U.cns(className, U.ifElse(isHomeTeam(team), "home-team", "enemy-team"))}>{U.view("name", team)}</span>
 }
 
