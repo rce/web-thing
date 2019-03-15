71768dbd220bd78344402858047cd94fa421a854
Henry Heikkinen <rce@rce.fi>
2019-03-15 11:55:01 +0200

    Prettier styles for player details

--- src/PlayerDetails.jsx
+++ src/PlayerDetails.jsx
@@ -12,21 +12,25 @@ const PlayerDetails = ({params: {playerId}}) => {
   const isOpen = new Atom(false)
 
   const steamId = U.view("steam_id_64", player)
-  return <div className="player-details">
+  return <div>
     {U.ifElse(player.map(R.isNil),
       <Spinner />,
       <div>
-        <h2>{U.view("nickname", player)} ({U.view("country", player)})</h2>
-        <img className="player-avatar" src={U.view("avatar", player)} />
-        {U.when(steamId, <p><a href={U.string`https://steamcommunity.com/profiles/${steamId}`}>Steam profile</a></p>)}
-        <table>
-          <tbody>
-            <tr><td>Player ID</td><td>{U.view("player_id", player)}</td></tr>
-            <tr><td>Country</td><td>{U.view("country", player)}</td></tr>
-            {U.when(player.map(R.path(["games", "csgo", "faceit_elo"])),
-              <tr><td>CSGO elo</td><td>{U.view(["games", "csgo", "faceit_elo"], player)}</td></tr>)}
-          </tbody>
-        </table>
+        <div className="player">
+          <img className="player-avatar" src={U.view("avatar", player)} />
+          <div className="player-info">
+            <h2>{U.view("nickname", player)} ({U.view("country", player)})</h2>
+            {U.when(steamId, <p><a href={U.string`https://steamcommunity.com/profiles/${steamId}`}>Steam profile</a></p>)}
+            <table>
+              <tbody>
+                <tr><td>Player ID</td><td>{U.view("player_id", player)}</td></tr>
+                <tr><td>Country</td><td>{U.view("country", player)}</td></tr>
+                {U.when(player.map(R.path(["games", "csgo", "faceit_elo"])),
+                  <tr><td>CSGO elo</td><td>{U.view(["games", "csgo", "faceit_elo"], player)}</td></tr>)}
+              </tbody>
+            </table>
+          </div>
+        </div>
 
         <div>
           <button onClick={() => isOpen.modify(R.not)}>Show details</button>
